1e9f6892204d0553a62d518dc9a3f0b735729ea5
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-09-15 09:11:10 +0300

    добавлены валидации полей

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,10 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_CHARACTER(100, "Недопустимый символ"),
+    EMPTY_LINE(101, "Недопустимые пустые поля"),
+    INVALID_LENGTH(102, "Недопустимая длина строки"),
+    INVALID_PHONE_NUMBER_START(103, "Недопустимый номер телефона"),
+    INVALID_SNILS(104, "Недопустимый номер снилс")
+
 }
--- exception/src/main/kotlin/Validator.kt
+++ exception/src/main/kotlin/Validator.kt
@@ -2,8 +2,73 @@ abstract class Validator<T> {
     abstract fun validate(value: T?): List<ErrorCode>
 }
 
+class NameValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val list = ArrayList<ErrorCode>()
+        val str = value?.trim()
+
+        if (str == null || str.isEmpty())
+            return listOf(ErrorCode.INVALID_CHARACTER)
+        if (str.length > 16)
+            list.add(ErrorCode.INVALID_LENGTH)
+        if (!str.matches("[ЁёА-я]+".toRegex()))
+            list.add(ErrorCode.INVALID_CHARACTER)
+        return list
+    }
+}
+
 class PhoneValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
-        return listOf(ErrorCode.INVALID_CHARACTER)
+        val list = ArrayList<ErrorCode>()
+        val str = value?.trim()
+
+        if (str == null || str.isEmpty())
+            return listOf(ErrorCode.EMPTY_LINE)
+        if (str.length != 11)
+            list.add(ErrorCode.INVALID_LENGTH)
+        if (!str.matches("\\d+".toRegex()))
+            list.add(ErrorCode.INVALID_CHARACTER)
+        if (!str.startsWith('7') && !value.startsWith('8'))
+            list.add(ErrorCode.INVALID_PHONE_NUMBER_START)
+        return list
+    }
+}
+
+class EmailValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val list = ArrayList<ErrorCode>()
+        val str = value?.trim()
+
+        if (str == null || str.isEmpty())
+            return listOf(ErrorCode.EMPTY_LINE)
+        if (str.length > 32)
+            list.add(ErrorCode.INVALID_LENGTH)
+        if (!str.matches("[A-Za-z]+@[A-Za-z]+\\.[A-Za-z]+".toRegex()))
+            list.add(ErrorCode.INVALID_CHARACTER)
+        return list
+    }
+}
+
+class SnilsValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val list = ArrayList<ErrorCode>()
+        val str = value?.trim()
+
+        if (str == null || str.isEmpty())
+            return listOf(ErrorCode.EMPTY_LINE)
+        if (str.length != 11)
+            list.add(ErrorCode.INVALID_LENGTH)
+        if (!str.matches("\\d+".toRegex()))
+            list.add(ErrorCode.INVALID_CHARACTER)
+
+        val controlNumberActual = str.substring(9, 11).toInt()
+        var controlNumberExpected = 0
+        for (i in 1..9)
+            controlNumberExpected += str[i - 1].digitToInt() * (10 - i)
+        controlNumberExpected %= 101
+        if (controlNumberExpected == 100) controlNumberExpected = 0
+        if (controlNumberActual != controlNumberExpected)
+            list.add(ErrorCode.INVALID_SNILS)
+        return list
     }
 }
