24f3b49c8c940f9b456a0a9a017a39350da51b9b
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-09-18 14:30:07 +0300

    finish tasks

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,47 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <A, B> compare(p1: Pair<A, B>, p2: Pair<A, B>): Boolean {
+    return p1.first == p2.first && p1.second == p2.second
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <Any: Comparable<Any>> countGreaterThan(anArray: Array<Any>, elem: Any): Int {
+    var count = 0
+    for (i in anArray.indices) {
+        if (anArray[i] > elem)
+        count++
+    }
+    return count
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T: Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
+    private val list: MutableList<T> = mutableListOf()
 
+    fun push(elem: T) {
+        list.add(elem)
+    }
+
+    fun pop(): T {
+        return list.removeAt(list.size - 1)
+    }
+
+    fun isEmpty(): Boolean {
+        return list.isEmpty()
+    }
+
+    fun peek(): T{
+        return list[list.size - 1]
+    }
 }
